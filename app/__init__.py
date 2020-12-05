88ac5f9e58f58aa3a921010b0b6f4860ecf0ab09
Kerrykogei24 <kerrykomar.gmail.com>
2020-12-05 14:17:58 +0300

    initializing blueprint

--- app/__init__.py
+++ app/__init__.py
@@ -8,8 +8,14 @@ from flask_mail import Mail
 
 
 
+login_manager = LoginManager()
+login_manager.session_protection = 'strong'
+login_manager.login_view = 'auth.login'
+
 db = SQLAlchemy()
 mail = Mail()
+bootstrap = Bootstrap()
+photos = UploadSet('photos',IMAGES)
 
 def create_app(config_name):
     app = Flask(__name__)
@@ -17,9 +23,18 @@ def create_app(config_name):
     # Creating the app configurations
     app.config.from_object(config_options[config_name])
 
+    # confiure UploadSet
+    configure_uploads(app,photos)
 
     # Initializing flask extensions
     db.init_app(app)
+    mail.init_app(app)
+    bootstrap.init_app(app)
+    login_manager.init_app(app)
+
+    # Registering the blueprint
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
 
 
     return app
