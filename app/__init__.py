c9f5ec6d60b760b288879279c89d43a9b43ebd87
Kerrykogei24 <kerrykomar.gmail.com>
2020-12-05 13:49:49 +0300

    create migration and initialize it

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,8 @@ from flask_sqlalchemy import SQLAlchemy
 from config import config_options
 from flask_login import LoginManager
 from flask_uploads import UploadSet, configure_uploads,IMAGES
+from flask_bootstrap import Bootstrap
+
 
 db = SQLAlchemy()
 
