9c59bdadfe2c9ea5b22d40b37eda6d27d87a7ace
Kerrykogei24 <kerrykomar.gmail.com>
2020-12-06 12:00:46 +0300

    created a markdown on form

--- app/__init__.py
+++ app/__init__.py
@@ -5,12 +5,15 @@ from flask_login import LoginManager
 from flask_uploads import UploadSet, configure_uploads,IMAGES
 from flask_bootstrap import Bootstrap
 from flask_mail import Mail
+from flask_simplemde import SimpleMDE
 
 
 
 login_manager = LoginManager()
 login_manager.session_protection = 'strong'
 login_manager.login_view = 'auth.login'
+
+simple = SimpleMDE()
 
 db = SQLAlchemy()
 mail = Mail()
@@ -31,6 +34,7 @@ def create_app(config_name):
     mail.init_app(app)
     bootstrap.init_app(app)
     login_manager.init_app(app)
+    simple.init_app(app)
 
     # Registering the blueprint
     from .main import main as main_blueprint
